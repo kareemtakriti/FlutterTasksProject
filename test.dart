e893702dfb451f32161b8f114f191d22d3c519be
kareemTakriti <kareemTakriti8@gmail.com>
2025-03-10 01:13:07 +0300

    testing

--- test.dart
+++ test.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 void main() async{
   File json = new File("test3.json");
   
-  await json.writeAsString(jsonEncode({"name" : "kareem Takriti"  , "id" : 1}));
+  await json.writeAsString(jsonEncode({"name" : "kareem Takriti bransh"  , "id" : 1}));
 }
 
 class TestClass{
