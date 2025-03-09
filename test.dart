da5972b354754f3c85eca4c136eff42b3a1ff974
kareemTakriti <kareemTakriti8@gmail.com>
2025-03-09 20:51:18 +0300

    Dart file edit json

---
+++ test.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() async{
+  File json = new File("test.json");
+  await json.writeAsString(jsonEncode({"name" : "kareem"  , "id" : "1"}));
+}
+
+class TestClass{
+  int id = 0; 
+  String name = "kareem"; 
+}
